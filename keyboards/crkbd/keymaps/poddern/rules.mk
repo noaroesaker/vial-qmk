22039e6a3bd1f8d1842ae0f3bbafed7ae6bf9bfb
poddern <64132067+noaroesaker@users.noreply.github.com>
2023-09-12 20:23:29 +0200

    ionnow hva som skjedde

--- keyboards/crkbd/keymaps/poddern/rules.mk
+++ keyboards/crkbd/keymaps/poddern/rules.mk
@@ -3,7 +3,7 @@ VIAL_ENABLE         = yes
 LTO_ENABLE          = yes
 
 RGBLIGHT_ENABLE     = no
-RGB_MATRIX_ENABLE   = yes # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+RGB_MATRIX_ENABLE   = no# Can't have RGBLIGHT and RGB_MATRIX at the same time.
 MOUSEKEY_ENABLE     = no
 OLED_ENABLE         = no
 OLED_DRIVER         = SSD1306
@@ -14,3 +14,4 @@ QMK_SETTINGS        = no
 
 BOOTLOADER 	= caterina
 NKRO_ENABLE = yes
+
