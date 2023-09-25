bcba1976a7637b333c59a16050568d86481492de
poddern <64132067+noaroesaker@users.noreply.github.com>
2023-09-25 20:31:27 +0200

    added herman vial to helix

---
+++ keyboards/helix/rev2/keymaps/herman/config.h
@@ -0,0 +1,29 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE4, 0x99, 0x35, 0x98, 0xBC, 0xC5, 0xA0, 0x11}
+#define VIAL_UNLOCK_COMBO_ROWS {4, 9}
+#define VIAL_UNLOCK_COMBO_COLS {4, 4}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+#define EE_HANDS
---
+++ keyboards/helix/rev2/keymaps/herman/rules.mk
@@ -0,0 +1,22 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = yes
+COMBO_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+OLED_ENABLE = no
+
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+
+LTO_ENABLE = yes
+
+KEY_OVERRIDE_ENABLE = no
+
+ifneq ($(strip $(KEYBOARD)), helix/rev2)
+   TAP_DANCE_ENABLE = no
+endif
+ifeq ($(strip $(KEYBOARD)), helix/rev2/qmk_conf)
+   QMK_SETTINGS = no
+endif
