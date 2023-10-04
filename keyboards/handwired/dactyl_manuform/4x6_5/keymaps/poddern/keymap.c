fea133417116a7b7f1f411f54318c32085e2e124
poddern <64132067+noaroesaker@users.noreply.github.com>
2023-10-04 16:17:08 +0200

    virker, men ikke via eller vial
    jeg vet ikke helt hva jeg ikker har gjort her, men den vil ikke vise seg fram på via, og vial gidder ikke bygge til uf2

--- keyboards/handwired/dactyl_manuform/4x6_5/keymaps/poddern/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6_5/keymaps/poddern/keymap.c
@@ -2,9 +2,14 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "keymap_norwegian.h"
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
+
+
+#define NO_OE     NO_OSTR                 // Ø //
+#define NO_AA     NO_ARNG                 // Å //
 
 enum layer_names {
     _BASE,
@@ -13,28 +18,13 @@ enum layer_names {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base (qwerty)
-     * +-----------------------------------------+                             +-----------------------------------------+
-     * | ESC  |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |      |
-     * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
-     * | TAB  |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |      |
-     * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
-     * | SHFT |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |      |
-     * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
-     *               |  [   |   ]  |                                                         |      |      |
-     *               +-------------+-------------+                             +-------------+-------------+
-     *                             |      |      |                             |      |      |
-     *                             +------+------|                             |------+------+
-     *                                    |      |                             |      |
-     *                                    +------+                             +------+
-     */
     [_BASE] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    NO_AA,
+        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    NO_OE,   NO_AE,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  NO_MINS, KC_ENT,
                           KC_LBRC, KC_RBRC,                                                KC_PLUS, KC_EQL,
-                                       KC_SPC,  KC_BSPC, KC_BSPC,                   KC_LGUI,  KC_ENT, KC_LGUI
-
+                                         KC_DEL,  KC_BSPC,               KC_SPC,  KC_LGUI,
+                                                    KC_NO,               KC_NO
     ),
 
     [_LOWER] = LAYOUT(
