b478abdd3fd0db32e3d87d7c53aecfecb1e16582
poddern <64132067+noaroesaker@users.noreply.github.com>
2023-10-04 15:39:16 +0200

    convert to rp2040

---
+++ keyboards/handwired/dactyl_manuform/4x6_5/keymaps/poddern/config.h
@@ -0,0 +1,33 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x5B, 0x12, 0x91, 0xD0, 0xBE, 0x7C, 0x17, 0xEB}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+#define MASTER_LEFT
+#define USB_MAX_POWER_CONSUMPTION 500
+#define FORCE_NKRO
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define TAPPING_TERM 150
+#define QUICK_TAP_TERM 0
---
+++ keyboards/handwired/dactyl_manuform/4x6_5/keymaps/poddern/rules.mk
@@ -0,0 +1,8 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE = yes
+LTO_ENABLE          = no
+QMK_SETTINGS = no
+
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = no# Can't have RGBLIGHT and RGB_MATRIX at the same time.
+SERIAL_DRIVER = vendor
