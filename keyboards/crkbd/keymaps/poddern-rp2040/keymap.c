b6ff0e6b62ed0e37ebb511c3fbf5710022f43db1
poddern <64132067+noaroesaker@users.noreply.github.com>
2023-10-23 22:07:00 +0200

    rot 270

--- keyboards/crkbd/keymaps/poddern-rp2040/keymap.c
+++ keyboards/crkbd/keymaps/poddern-rp2040/keymap.c
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Rotate OLED
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    return OLED_ROTATION_90;
+    return OLED_ROTATION_270;
 }
 
 // Animation parameters
