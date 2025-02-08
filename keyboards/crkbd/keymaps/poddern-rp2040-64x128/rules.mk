2857fc4a881c6052717711276cf06a4d3bddebf7
poddern <64132067+noaroesaker@users.noreply.github.com>
2025-02-08 22:52:22 +0100

    initial update

---
+++ keyboards/crkbd/keymaps/poddern-rp2040-64x128/rules.mk
@@ -0,0 +1,21 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = no
+
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = yes
+
+OLED_ENABLE    = yes
+OLED_TRANSPORT = i2c
+
+NKRO_ENABLE     = yes
+EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = no
+COMBO_ENABLE    = no
+
+QMK_SETTINGS        = no
+
+BOOTLOADER 	= rp2040
+
+SERIAL_DRIVER= vendor
+WS2812_DRIVER = vendor
